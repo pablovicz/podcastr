f5a8929de3f71df50791534239c5084fbeea33e8
Pablo <pablowoina2205@gmail.com>
2021-04-21 11:23:45 -0300

    Aula 02 - parte 2

--- podcastr-frontend/src/pages/index.tsx
+++ podcastr-frontend/src/pages/index.tsx
@@ -1,7 +1,47 @@
+// 1 - SPA
+// 2 - SSR
+// 3 - SSG
+
+//1 - import { useEffect } from "react"
 
 
-export default function Home() {
+export default function Home(props) {
+
+  // 1 - SPA
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/apisodes')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }, [])
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
+}
+
+// 2 - SSR - executa toda vez que alguem acessa a home da aplicacao
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+//   return { 
+//     props: { // sempre props
+//       episodes: data, //qualquer nome
+//     }
+//   }
+// }
+
+
+// 3 - SSG - executa toda vez que alguem acessa a home da aplicacao - só funciona em produção
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return { 
+    props: { // sempre props
+      episodes: data, //qualquer nome
+    },
+    revalidate: 60*60*8 // tempo em segundos para que uma nova versão da pagina seja gerada
+  }
 }
