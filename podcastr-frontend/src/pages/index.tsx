67e8f533ac5ea0c52f14ac3fa7eb832e03b4b857
Pablo <pablowoina2205@gmail.com>
2021-04-23 16:53:52 -0300

    Aula 03 - parte 2

--- podcastr-frontend/src/pages/index.tsx
+++ podcastr-frontend/src/pages/index.tsx
@@ -1,5 +1,11 @@
 import { GetStaticProps } from 'next';
+import Image from 'next/image';
 import { api } from '../services/api';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { convertDurationToTime } from '../utils/convertDurationToTimeString';
+
+import styles from './home.module.scss';
 
 // 1 - SPA
 // 2 - SSR
@@ -10,15 +16,21 @@ import { api } from '../services/api';
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
+  description: string;
   members: string;
-
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
 }
 
 type HomeProps = {
-  episodes: Episode[]
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes } : HomeProps) {
 
   // 1 - SPA
   // useEffect(() => {
@@ -29,10 +41,41 @@ export default function Home(props: HomeProps) {
 
 
   return (
-    <>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
-    </>
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos Lançamentos</h2>
+        <ul>
+          {
+          latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image 
+                  width={192} 
+                  height={192} 
+                  src={episode.thumbnail} 
+                  alt={episode.title} 
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio"/>
+                </button>
+
+              </li>
+            )
+          }
+          )
+          }
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}></section>
+    </div>
   )
 }
 
@@ -50,7 +93,7 @@ export default function Home(props: HomeProps) {
 
 // 3 - SSG - executa toda vez que alguem acessa a home da aplicacao - só funciona em produção
 export const getStaticProps : GetStaticProps = async () => {
-  const { data } = await api.get('/apisodes', { 
+  const { data } = await api.get('/episodes', { 
     params: { 
       _limit: 12,
       _sort:'published_at',
@@ -58,9 +101,28 @@ export const getStaticProps : GetStaticProps = async () => {
     }
   })
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTime(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+
+    };
+  })
+
+  const latestEpisodes = episodes.slice(0,2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
   return { 
     props: { // sempre props
-      episodes: data, //qualquer nome
+      latestEpisodes: latestEpisodes, //qualquer nome
+      allEpisodes: allEpisodes, 
     },
     revalidate: 60*60*8 // tempo em segundos para que uma nova versão da pagina seja gerada
   }
---
+++ podcastr-frontend/src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,12 @@
+export function convertDurationToTime(duration: number) {
+  const hours = Math.floor(duration /3600);
+  const minutes = Math.floor((duration % 3600) /60) // resto da divisao
+  const seconds = duration % 60
+
+  const timeString = [hours, minutes, seconds]
+                          .map(unit => String(unit).padStart(2, '0'))
+                          .join(':')
+
+  return timeString;
+
+}
